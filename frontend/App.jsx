672625d5eeeab949655443c0d0c9a55b6ba26980
Aryan Rajput <113825530+Aaru607@users.noreply.github.com>
2025-12-29 16:45:48 +0530

    Create App component

---
+++ frontend/App.jsx
@@ -0,0 +1,13 @@
+import CareerAssessment from "./pages/CareerAssessment";
+import AdminUpload from "./pages/AdminUpload";
+
+function App() {
+  return (
+    <div>
+      <CareerAssessment />
+      <AdminUpload />
+    </div>
+  );
+}
+
+export default App;
